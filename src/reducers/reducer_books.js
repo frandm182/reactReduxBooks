277c303e9556249e6947c9b17705f290c48c08c2
frandm182 <franjdm@gmail.com>
2017-04-12 14:31:31 +0200

    Add files via upload

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+	return [
+		{ title: 'Javascript: the good parts', pages: 101},
+		{ title: 'Harry Potter', pages: 58},
+		{ title: 'The Dark Tower', pages: 85},
+		{ title: 'Hola', pages: 256},
+		{ title: 'Eloquent', pages: 159}
+	]
+}
